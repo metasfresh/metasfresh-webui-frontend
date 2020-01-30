257b57f6a5268ec2ace31c86a1ee089003ce722c
siemiatj <kuba@saskla.cz>
2020-01-30 01:07:33 +0100

    #2517 some progress with WindowActions test #2517 update fixtures

--- src/__tests__/actions/WindowActions.test.js
+++ src/__tests__/actions/WindowActions.test.js
@@ -1,6 +1,7 @@
 import thunk from 'redux-thunk'
 import nock from 'nock';
 import configureStore from 'redux-mock-store';
+import { Set } from 'immutable';
 
 import {
   updateTabRowsData,
@@ -12,11 +13,13 @@ import {
 } from '../../actions/WindowActions';
 import * as ACTION_TYPES from '../../constants/ActionTypes';
 
-import fixtures from "../../../test_setup/fixtures/master_window.json";
+import masterWindowProps from '../../../test_setup/fixtures/master_window.json';
+import dataFixtures from '../../../test_setup/fixtures/master_window/data.json';
+import layoutFixtures from '../../../test_setup/fixtures/master_window/layout.json';
 
 describe('WindowActions synchronous', () => {
   it('should return a UPDATE_TAB_ROWS_DATA action with correct payload', () => {
-    const payload = { 'AD_Tab-1': { changed: { id: 1 }, removed: {} }};
+    const payload = { 'AD_Tab-1': { changed: { id: 1 } }};
     const action = updateTabRowsData('master', 'AD_Tab-1', payload);
 
     expect(action.type).toEqual(ACTION_TYPES.UPDATE_TAB_ROWS_DATA)
@@ -25,33 +28,74 @@ describe('WindowActions synchronous', () => {
   });
 });
 
-describe.skip('WindowActions thunks', () => {
-  const fixturesData = fixtures.data1;
+describe('WindowActions thunks', () => {
+  const propsData = masterWindowProps.data1;
   const middlewares = [thunk];
   const mockStore = configureStore(middlewares);
 
   describe('init', () => {
     afterEach(() => {
-      fetchMock.restore()
+      nock.restore();
     });
 
     it(`dispatches 'initWindow' and 'initDataSuccess' action creators`, () => {
       const store = mockStore();
-      const { windowType, docId } = fixturesData;
+      const { params: { windowType, docId } } = propsData;
+      const dataResp = {
+        data: {},
+        docId: undefined,
+        includedTabsInfo: undefined,
+        scope: 'master',
+        saveStatus: undefined,
+        standardActions: undefined,
+        validStatus: undefined,
+        websocket: undefined,
+      }
 
       nock(config.API_URL)
         .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(`rest/api/window/${windowType}/${docId}`)
-        .reply(200, { data: { windowId: windowType } });
+        .get(`/window/${windowType}/${docId}/`)
+        .reply(200, [{ fieldsByName: {} }]);
 
       const expectedActions = [
         { type: ACTION_TYPES.INIT_WINDOW },
-        { type: ACTION_TYPES.INIT_DATA_SUCCESS, body: { windowId: windowType } }
+        { type: ACTION_TYPES.INIT_DATA_SUCCESS, ...dataResp }
       ]
 
       return store.dispatch(createWindow(windowType, docId, undefined, undefined, false)).then(() => {
         expect(store.getActions()).toEqual(expectedActions)
       });
     });
+
+    it(`'initDataSuccess'`, () => {
+      const store = mockStore();
+      const { params: { windowType, docId } } = propsData;
+      const notFoundResp = {
+        data: {},
+        docId: 'notfound',
+        includedTabsInfo: {},
+        scope: 'master',
+        saveStatus: { saved: true },
+        standardActions: Set(),
+        validStatus: {},
+      }
+
+      nock(config.API_URL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+        .get(`/window/${windowType}/${docId}/`)
+        .reply(404);
+
+      const expectedActions = [
+        { type: ACTION_TYPES.INIT_WINDOW },
+        { type: ACTION_TYPES.INIT_DATA_SUCCESS, ...notFoundResp }
+      ]
+
+      return store.dispatch(createWindow(windowType, docId, undefined, undefined, false)).then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      });
+    });
+
+    // @TODO: tests for NEW windows, NEW rows
   });
 })
+
