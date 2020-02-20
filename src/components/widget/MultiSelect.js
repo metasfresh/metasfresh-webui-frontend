85e1c6e96d2081cb6e48c51ce15eb883e4b513ee
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-02-20 08:58:20 +0200

    More refactoring moved styles to css file #2607

--- src/components/widget/MultiSelect.js
+++ src/components/widget/MultiSelect.js
@@ -5,22 +5,10 @@ class MultiSelect extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isSelectBoxVisible: true,
       data: this.props.options,
       filter: '',
       checkedItems: {},
-      checkboxSelectAll: false,
-      activeSearch: false,
     };
-  }
-
-  static getDerivedStateFromProps(nextProps, nextState) {
-    if (!nextState.activeSearch) {
-      return {
-        data: nextProps.options,
-      };
-    }
-    return null;
   }
 
   componentDidMount() {
@@ -47,44 +35,34 @@ class MultiSelect extends Component {
   };
 
   render() {
-    const { isSelectBoxVisible, data } = this.state;
+    const { data } = this.state;
     const dataSource = data.size > 0 ? data : this.props.options;
 
-    const style = {
-      mainBoxStyle: {
-        width: '100%',
-        minHeight: '30px',
-        padding: '15px',
-      },
-    };
-
     return (
-      <div style={style.mainBoxStyle}>
-        {isSelectBoxVisible && (
-          <div>
-            {dataSource.map((item) => (
-              <div className="form-group" key={item.key}>
-                <div key={item.key} className="row">
-                  <div className=" col-sm-6 float-left">
-                    <label className="form-control-label" title={item.caption}>
-                      {item.caption}
-                    </label>
-                  </div>
+      <div className="filter-multiselect">
+        <div>
+          {dataSource.map((item) => (
+            <div className="form-group" key={item.key}>
+              <div key={item.key} className="row">
+                <div className=" col-sm-6 float-left">
+                  <label className="form-control-label" title={item.caption}>
+                    {item.caption}
+                  </label>
+                </div>
 
-                  <div className="col-sm-6 float-right">
-                    <label className="input-checkbox">
-                      <input
-                        type="checkbox"
-                        onChange={() => this.selectItem(item.key, item.caption)}
-                      />
-                      <span className="input-checkbox-tick" />
-                    </label>
-                  </div>
+                <div className="col-sm-6 float-right">
+                  <label className="input-checkbox">
+                    <input
+                      type="checkbox"
+                      onChange={() => this.selectItem(item.key, item.caption)}
+                    />
+                    <span className="input-checkbox-tick" />
+                  </label>
                 </div>
               </div>
-            ))}
-          </div>
-        )}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
