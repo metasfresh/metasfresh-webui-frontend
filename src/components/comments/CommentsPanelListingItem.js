7ba97968839318a91bd071909ab2821d00272321
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-04-28 12:44:23 +0300

    Layout changes boxes #6504

--- src/components/comments/CommentsPanelListingItem.js
+++ src/components/comments/CommentsPanelListingItem.js
@@ -10,21 +10,23 @@ const CommentsPanelListingItem = (props) => {
     <div className="col-lg-12">
       <div className="row">
         <div className="panel panel-spaced panel-distance panel-bordered panel-primary">
-          <div className="col-lg-1 float-left">&nbsp;</div>
-          <div className="col-lg-11 float-right">{text}</div>
+          <div className="col-lg-1 col-md-4 col-sm-6 float-left">&nbsp;</div>
+          <div className="col-lg-11 col-md-8 col-sm-6 float-right">{text}</div>
           <div className="clearfix" />
 
-          <div className="col-lg-1 float-left">
+          <div className="col-lg-1 col-md-4 col-sm-6 float-left">
             {counterpart.translate('view.about.created')}
           </div>
-          <div className="col-lg-11 float-right">
+          <div className="col-lg-11 col-md-8 col-sm-6 float-right">
             {Moment(created).format(`${DATE_FORMAT} ${TIME_FORMAT}`)}
           </div>
           <div className="clearfix" />
-          <div className="col-lg-1 float-left">
+          <div className="col-lg-1 col-md-4 col-sm-6 float-left">
             {counterpart.translate('view.about.createdBy')}
           </div>
-          <div className="col-lg-11 float-right">{createdBy}</div>
+          <div className="col-lg-11 col-md-8 col-sm-6 float-right">
+            {createdBy}
+          </div>
         </div>
       </div>
     </div>
