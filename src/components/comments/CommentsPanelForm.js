e7147abec0aa6db72ae595884584222d40474ed0
Petrica Nanca <petrica.nanca@metasfresh.com>
2020-04-30 07:38:13 +0300

    Added form element #6504

--- src/components/comments/CommentsPanelForm.js
+++ src/components/comments/CommentsPanelForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, Fragment } from 'react';
 import { callAPI } from '../../actions/WindowActions';
 import { updateCommentsPanelTextInput } from '../../actions/CommentsPanelActions';
 import { connect } from 'react-redux';
@@ -18,52 +18,55 @@ export const CommentsPanelForm = (props) => {
   }, []);
 
   return (
-    <div className="col-12">
-      <div className="panel panel-spaced panel-distance panel-bordered panel-primary">
-        <div className="row">
-          <div className="col-lg-10 col-md-10 col-sm-10 float-left">
-            <div className="input-body-container">
-              <span />
-              <div>
-                <div className="input-block input-primary pulse-off">
-                  <textarea
-                    ref={textRef}
-                    autoComplete="off"
-                    className="input-field js-input-field"
-                    onKeyPress={(e) => updateText(e.target.value)}
-                    onChange={(e) => updateText(e.target.value)}
-                    tabIndex="0"
-                    value={textInput}
-                  />
+    <Fragment>
+      <form className="col-12">
+        <div className="panel panel-spaced panel-distance panel-bordered panel-primary">
+          <div className="row">
+            <div className="col-lg-10 col-md-10 col-sm-10 float-left">
+              <div className="input-body-container">
+                <span />
+                <div>
+                  <div className="input-block input-primary pulse-off">
+                    <textarea
+                      ref={textRef}
+                      autoComplete="off"
+                      className="input-field js-input-field"
+                      onKeyPress={(e) => updateText(e.target.value)}
+                      onChange={(e) => updateText(e.target.value)}
+                      tabIndex="0"
+                      value={textInput}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="col-lg-2 col-md-2 col-sm-2 float-right">
+              <button
+                className="btn btn-sm btn-block btn-meta-success"
+                onClick={(e) => {
+                  e.preventDefault();
+                  !textInput && textRef.current.focus();
+                  textInput &&
+                    postComment({
+                      windowId,
+                      docId,
+                      tabId: null,
+                      rowId: null,
+                      target: 'comments',
+                      verb: 'POST',
+                      data: textInput,
+                    });
+                }}
+                tabIndex={0}
+              >
+                {sendButton}
+              </button>
+            </div>
+            <div className="clearfix" />
           </div>
-          <div className="col-lg-2 col-md-2 col-sm-2 float-right">
-            <button
-              className="btn btn-sm btn-block btn-meta-success"
-              onClick={() => {
-                !textInput && textRef.current.focus();
-                textInput &&
-                  postComment({
-                    windowId,
-                    docId,
-                    tabId: null,
-                    rowId: null,
-                    target: 'comments',
-                    verb: 'POST',
-                    data: textInput,
-                  });
-              }}
-              tabIndex={0}
-            >
-              {sendButton}
-            </button>
-          </div>
-          <div className="clearfix" />
         </div>
-      </div>
-    </div>
+      </form>
+    </Fragment>
   );
 };
 
